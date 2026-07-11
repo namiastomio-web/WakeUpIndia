0dfe6055d03d2f1f00ea27424d6973a9972d2a3a
Namiastomio <namiastomio@gmail.com>
2026-07-12 00:10:44 +0530

    Add goToYear function for year navigation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("welcome to wakeUpIndia");
+
+<script>
+  function goToYear()
+{
+  const year =
+  document.getElementById("year").value;
+    window.location.href = "Kalahandi/" + year + ".html";
+  }
+  </script>
