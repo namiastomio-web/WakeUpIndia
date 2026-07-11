da0e0e31180d04d251b5582ee2dc71ee008bee85
Namiastomio <namiastomio@gmail.com>
2026-07-12 00:14:08 +0530

    Fix console log statement in script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 console.log("welcome to wakeUpIndia");
-
-<script>
-  function goToYear()
-{
-  const year =
-  document.getElementById("year").value;
-    window.location.href = "Kalahandi/" + year + ".html";
-  }
-  </script>
