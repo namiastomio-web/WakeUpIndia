2c83b790deb7a14283bc29cb68dbd523c2161138
Namiastomio <namiastomio@gmail.com>
2026-07-12 17:40:58 +0530

    Refactor year selection event listener

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 console.log("welcome to wakeUpIndia");
 
-document.getElementById("yearselect")
-.addEventListener("change", function ()
-                  {
-                      const year = this.value;
+document.getElementById("year").addEventListener("change", function () {
+    const year = this.value;
 
-                  if (year === "2026") {
-                      window.location.href =
-                          "kalahandi/2026.html";
-                  }
-                  });
+    if (year === "2026") {
+        window.location.href = "kalahandi/2026.html";
+    }
+});
